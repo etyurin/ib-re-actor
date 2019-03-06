ae577e735b1359bf18e9a80cb864053e3c2bc0f0
Eugene Tyurin <eugene@tyurin.com>
2019-03-06 16:45:06 -0600

    updated versions including clojure 1.10

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject ib-re-actor "0.2.0-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-time "0.14.2"]
-                 [org.clojure/tools.logging "0.4.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-time "0.15.1"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [clj-logging-config "1.9.12"]]
   :profiles {:dev {:dependencies [[twsapi "9.71.01"]
-                                  [midje "1.9.1"]]
+                                  [midje "1.9.6"]]
                    :plugins [[lein-midje "3.2.1"]
                              [com.gfredericks/how-to-ns "0.1.6"]]
                    :how-to-ns {:require-docstring? false}}})
